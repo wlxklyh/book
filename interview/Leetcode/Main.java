872d7400742ab7e4555195fb3501bf9139ffd650
wlxklyh <linyanhou@qq.com>
2020-12-14 12:49:00 +0800

    同步
    同步

--- interview/Leetcode/Main.java
+++ interview/Leetcode/Main.java
@@ -1,1 +1,12 @@
-class 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque deque = new ArrayDeque();
+        deque.push(1);
+        deque.add(1);
+        
+        
+    }
+}
